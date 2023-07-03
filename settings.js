93ea96dacb169dfb0c4efe892917f43597d13001
vihaga122 <134380036+vihaga122@users.noreply.github.com>
2023-07-03 19:01:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,7 +29,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['94741842307'] // owner number
+global.vcardowner = ['+94741842307'] // owner number
 global.ownername = "*MR Vihaga Chamod*❯❯" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: vihaga" // github or insta name
@@ -47,8 +47,8 @@ global.upsongvoice = "true" // true/false \\ bot can upload song as voice note (
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['94741842307']
-global.ownertag = '94741842307' //your tag number
+global.owner = ['+94741842307']
+global.ownertag = '+94741842307' //your tag number
 global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
 global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
@@ -56,7 +56,7 @@ global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['94741842307'] //ur premium numbers
+global.premium = ['+94741842307'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
